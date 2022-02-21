a0dbdb9eae0fd393b6107e20e29d91b7f8fb4406
zhengliang.zhang <zhengliang.zhang@united-imaging.com>
2022-02-21 10:31:38 +0800

    添加首页图表

--- src/components/global/apexcharts.vue
+++ src/components/global/apexcharts.vue
@@ -1,103 +1,27 @@
 <template>
-  <div id="chart" ref="ccc"></div>
+  <div id="chart"></div>
 </template>
 
 <script>
 import * as ApexCharts from "apexcharts";
-import { defineComponent, ref,onMounted } from "vue";
+import { defineComponent, onMounted,nextTick } from "vue";
 export default defineComponent({
-  setup() {
-
-    var options = {
-      series: [
-        {
-          name: "TEAM A",
-          type: "column",
-          data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
-        },
-        {
-          name: "TEAM B",
-          type: "area",
-          data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
-        },
-        {
-          name: "TEAM C",
-          type: "line",
-          data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
-        },
-      ],
-      chart: {
-        height: 350,
-        type: "line",
-        stacked: false,
-      },
-      stroke: {
-        width: [0, 2, 5],
-        curve: "smooth",
-      },
-      plotOptions: {
-        bar: {
-          columnWidth: "50%",
-        },
-      },
-
-      fill: {
-        opacity: [0.85, 0.25, 1],
-        gradient: {
-          inverseColors: false,
-          shade: "light",
-          type: "vertical",
-          opacityFrom: 0.85,
-          opacityTo: 0.55,
-          stops: [0, 100, 100, 100],
-        },
-      },
-      labels: [
-        "01/01/2003",
-        "02/01/2003",
-        "03/01/2003",
-        "04/01/2003",
-        "05/01/2003",
-        "06/01/2003",
-        "07/01/2003",
-        "08/01/2003",
-        "09/01/2003",
-        "10/01/2003",
-        "11/01/2003",
-      ],
-      markers: {
-        size: 0,
-      },
-      xaxis: {
-        type: "datetime",
-      },
-      yaxis: {
-        title: {
-          text: "Points",
-        },
-        min: 0,
-      },
-      tooltip: {
-        shared: true,
-        intersect: false,
-        y: {
-          formatter: function (y) {
-            if (typeof y !== "undefined") {
-              return y.toFixed(0) + " points";
-            }
-            return y;
-          },
-        },
-      },
-    };
-
+  props: {
+    options: {
+      type: Object,
+      required: true 
+    }
+  }, 
+  setup(props) {
     // 使用ref
     onMounted(() => {
-      var chartDom = document.querySelector('#chart')
+      var chartDom = document.querySelector("#chart");
 
-      var chart = new ApexCharts(chartDom, options);
+      var chart = new ApexCharts(chartDom, props.options);
 
-      chart.render();
+      nextTick(() => {
+          chart.render()
+      })
     });
 
     return {};
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,16 +1,115 @@
 <template>
   <div class="home">
-    <apexcharts></apexcharts>
+    <a-row>
+      <a-col :span="24">
+        <apexcharts :options="options" />
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script >
-import { defineComponent} from "vue";
+import { defineComponent ,ref} from "vue";
 import { useRouter } from 'vue-router'
 export default defineComponent({
   name: "Home",
   setup() {
     const router = useRouter()
+
+    const options = ref({
+      series: [
+        {
+          name: "Type A",
+          type: "column",
+          data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+        },
+        {
+          name: "Type B",
+          type: "area",
+          data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+        },
+        {
+          name: "Type C",
+          type: "line",
+          data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+        },
+      ],
+      chart: {
+        height: 350,
+        type: "line",
+        stacked: false,
+      },
+      stroke: {
+        width: [0, 2, 5],
+        curve: "smooth",
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: "50%",
+        },
+      },
+      fill: {
+        opacity: [0.85, 0.25, 1],
+        gradient: {
+          inverseColors: false,
+          shade: "light",
+          type: "vertical",
+          opacityFrom: 0.85,
+          opacityTo: 0.55,
+          stops: [0, 100, 100, 100],
+        },
+      },
+      labels: [
+        "01/01/2022",
+        "02/01/2022",
+        "03/01/2022",
+        "04/01/2022",
+        "05/01/2022",
+        "06/01/2022",
+        "07/01/2022",
+        "08/01/2022",
+        "09/01/2022",
+        "10/01/2022",
+        "11/01/2022",
+      ],
+      markers: {
+        size: 0,
+      },
+      xaxis: {
+        type: "datetime",
+      },
+      yaxis: {
+        title: {
+          text: "Points",
+        },
+        splitLine: {
+          show: false,
+        },
+        min: 0,
+      },
+      tooltip: {
+        shared: true,
+        intersect: false,
+        y: {
+          formatter: function (y) {
+            if (typeof y !== "undefined") {
+              return y.toFixed(0) + " points";
+            }
+            return y;
+          },
+        },
+      },
+      toolbar: {
+        show: false,
+      },
+      grid: {
+        clipMarkers: false,
+      },
+    })
+
+    return {
+      options
+    }
   }
 });
 </script>
